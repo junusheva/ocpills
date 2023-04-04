1d52d3d49d2673cb8935ade8cfa9f1410e1ef6a1
Aijan <aikojune@gmail.com>
2023-04-04 23:38:03 +0600

    Use tailwind css

--- App.js
+++ App.js
@@ -1,11 +1,11 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { Text, View } from "react-native";
 import { Input } from "@rneui/themed";
 import Icon from "react-native-vector-icons/MaterialCommunityIcons";
 
 export default function App() {
   return (
-    <View style={styles.container}>
+    <View className="flex-1 items-center justify-center p-10">
       <Input
         containerStyle={{}}
         disabledInputStyle={{ background: "#ddd" }}
@@ -39,12 +39,3 @@ export default function App() {
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
